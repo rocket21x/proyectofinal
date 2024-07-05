bc9fa6be277429382d024e843e25aa67ad9f49de
Rodrigo Gtz <rigo_22mayo@hotmail.com>
2024-07-05 06:57:28 -0700

    sss
    sss

---
+++ PresentacionProyecto/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+
+package main;
+
+import negocio.ControladorAdministracionSistema;
+
+public class Main {
+    
+     public static void main(String[] args) {
+        
+         ControladorAdministracionSistema controladorAdministracionSistema = new ControladorAdministracionSistema();
+         controladorAdministracionSistema.abrirInicioSesion();
+         
+    }
+     
+}
