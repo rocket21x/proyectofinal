0c5dfaaba0e98a8ca8a5655c785868d6eb4fcbf8
jesus imperial <jeskid99@gmail.com>
2024-07-05 13:36:15 -0700

    final

--- PresentacionProyecto/src/main/java/main/Main.java
+++ PresentacionProyecto/src/main/java/main/Main.java
@@ -10,6 +10,7 @@ public class Main {
          ControladorAdministracionSistema controladorAdministracionSistema = new ControladorAdministracionSistema();
          controladorAdministracionSistema.abrirInicioSesion();
          
+         
     }
      
 }
